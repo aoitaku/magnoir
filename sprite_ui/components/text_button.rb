18b760de9b8b7060e12b36e32d49615ae8c1bc1a
aoitaku <aoitaku@gmail.com>
2014-12-14 20:28:38 +0900

    add enable/disable switching function to button

--- sprite_ui/components/text_button.rb
+++ sprite_ui/components/text_button.rb
@@ -1,13 +1,36 @@
 class UI::TextButton < UI::TextLabel
 
+  attr_writer :color
+
   def draw_params
-    if active?
-      [text, {color: [255,127,255,223]}]
-    elsif hover?
-      [text, {color: [255,255,223,127]}]
+    if color
+      [text, {color: color}]
     else
       [text, {}]
     end
   end
 
+  def disable?
+    @disable
+  end
+
+  def enable
+    @disable = false
+  end
+
+  def disable
+    @disable = true
+  end
+
+  def color
+    if disable?
+      @color or [255,127,127,127]
+    elsif active?
+      [255,127,255,223]
+    elsif hover?
+      [255,255,223,127]
+    else
+      @color
+    end
+  end
 end
