00de634eafbf0f65ef68a2385a31b4f018410080
aoitaku <aoitaku@gmail.com>
2014-12-14 20:29:29 +0900

    modified game view with using sprite ui

--- view.rb
+++ view.rb
@@ -58,43 +58,214 @@ end
 
 class GameView < ViewBase
 
+  def initialize(model, controller)
+    super
+    @pause = UI::TextLabel.new
+    @pause.font = Font20
+    @pause.text = "PAUSE"
+    @pause.x = 20
+    @pause.y = 15
+    @line = Sprite.new(480, 20, Image.new(1,420).line(0,0,0,420,[255,255,255]))
+    ship_caption = proc {
+      image Image.new(100, 30).box(0, 0, 100, 30, [255,255,255])
+      TextLabel(:lv) {
+        x 4
+        y 7
+        text "Lv.0"
+        font Font16
+        position :absolute
+      }
+      TextButton(:gradeup) {
+        x 45
+        y 10
+        width 48
+        height 12
+        font Font12
+      }
+    }
+    ship_state = proc {
+      y 5
+      image Image.new(100, 100).box(0, 0, 100, 100, [255,255,255])
+      TextLabel(:stat) {
+        x 6
+        y 10
+        font Font16
+      }
+    }
+    @ui = SpriteUI.build {
+      ContainerBox(:warehouse) {
+        x 490
+        position :absolute
+        ContainerBox(:fishes) {
+          y 90
+          position :absolute
+        }
+        TextLabel(:money) {
+          y 400
+          text "資金 600"
+          font Font20
+          position :absolute
+        }
+        TextLabel(:day) {
+          y 20
+          text "1日目"
+          font Font20
+          position :absolute
+        }
+        TextLabel(:time) {
+          y 40
+          text "0:00"
+          font Font20
+          position :absolute
+        }
+        TextLabel(:rate) {
+          y 60
+          text "相場 10"
+          font Font20
+          position :absolute
+        }
+      }
+      ContainerBox(:ships) {
+        ContainerBox {
+          x 20
+          y 40
+          ContainerBox(&ship_caption)
+          ContainerBox(&ship_state)
+        }
+        ContainerBox {
+          x 130
+          y 40
+          ContainerBox(&ship_caption)
+          ContainerBox(&ship_state)
+        }
+        ContainerBox {
+          x 240
+          y 40
+          ContainerBox(&ship_caption)
+          ContainerBox(&ship_state)
+        }
+        ContainerBox {
+          x 350
+          y 40
+          ContainerBox(&ship_caption)
+          ContainerBox(&ship_state)
+        }
+      }
+    }
+    @ui.layout
+    @mouse_event_dispatcher = SpriteUI::MouseEventDispatcher.new(@ui)
+  end
+
+  def fish_container
+    SpriteUI.build {
+      height 20
+      TextButton(:amount) {
+        width 100
+        height 20
+        font Font20
+        position :absolute
+      }
+      TextLabel(:gauge) {
+        x 90
+        font Font20
+        position :absolute
+      }
+    }
+  end
+
+  def update
+    @mouse_event_dispatcher.update
+    fishes = @ui.find(:fishes)
+    if @model.fishes.size > fishes.components.size
+      (@model.fishes.size - fishes.components.size).times do |i|
+        fishes.add(fish_container)
+      end
+      fishes.layout
+    elsif @model.fishes.size < fishes.components.size
+      fishes.components.pop(fishes.components.size - @model.fishes.size)
+      fishes.layout
+    end
+    fishes.components.each_with_index do |fish, n|
+      fish.find(:amount).text = fish_text(@model.fishes[n])
+      if @model.inbusiness?
+        fish.find(:amount).enable
+      else
+        fish.find(:amount).disable
+      end
+      fish.find(:gauge).text = fish_gauge(@model.fishes[n])
+    end
+    @ui.find(:ships).components.each_with_index do |ship, n|
+      ship.find(:lv).text = lv_text(n)
+      ship.find(:gradeup).text = ship_caption_text(n)
+      if @model.infishing?
+        ship.find(:gradeup).color = nil
+        ship.find(:gradeup).disable
+      elsif @model.money < @model.ships[n].levelup_cost
+        ship.find(:gradeup).color = [255,255,63,0]
+        ship.find(:gradeup).disable
+      else
+        ship.find(:gradeup).color = nil
+        ship.find(:gradeup).enable
+      end
+      ship.find(:stat).text = ship_state_text(n)
+    end
+    @ui.find(:money).text = money_text
+    @ui.find(:day).text = day_text
+    @ui.find(:time).text = time_text
+    @ui.find(:rate).text = rate_text
+    @mouse_event_dispatcher.dispatch
+  end
+
   def draw
-    Window.drawFont(20,15,"PAUSE",Font20) if(@model.pause)
-    Window.draw(WAREHOUSE_X,20,WAREHOUSE_LINE)
-    @model.fishes.each_with_index do |f,i|
-      Window.drawFont(WAREHOUSE_X+10,FISH_Y+i*20,"マグロ "+f.amount.to_s,Font20)
-      Window.drawFont(WAREHOUSE_X+100,FISH_Y+i*20,f.limit_gage,Font20)
-    end
-    Window.drawFont(WAREHOUSE_X+10,400,"資金 "+@model.money.to_s,Font20)
-    Window.drawFont(WAREHOUSE_X+10,20,@model.day.to_s+"日目",Font20)
-    Window.drawFont(WAREHOUSE_X+10,40,@model.hour.to_s+":00",Font20)
-    fonthash = {}
-    fonthash = {color: YELLOW} if(@model.inbusiness?)
-    Window.drawFont(WAREHOUSE_X+10,60,"相場 "+@model.rate.to_s,Font20,fonthash)
-    4.times do |i|
-      Window.draw(SHIPS_X[i],SHIPS_ALT_Y,SHIPS_ALT[i])
-      Window.draw(SHIPS_X[i],SHIPS_Y,SHIPS[i])
-      draw_ship_alt(i)
-      draw_ship(i)
-    end
+    @pause.draw if @model.pause
+    @line.draw
+    @ui.draw
   end
 
-  def draw_ship_alt(n)
-    Window.drawFont(SHIPS_X[n]+4,SHIPS_ALT_Y+7,"Lv."+@model.ships[n].level.to_s,Font16)
-    alt_str = @model.ships[n].level > 0 ? "改造" : "購入"
-    fonthash = {}
-    fonthash = {color: RED} if(@model.money < @model.ships[n].levelup_cost)
-    fonthash = {color: GRAY} if(@model.infishing?)
-    Window.drawFont(SHIPS_X[n]+45,SHIPS_ALT_Y+10,alt_str+@model.ships[n].levelup_cost.to_s,Font12,fonthash)
+  def fish_text(f)
+    "マグロ #{f.amount.to_s}"
   end
 
-  def draw_ship(n)
-    if(@model.ships[n].level==0)
-      ship_status = ""
+  def fish_gauge(f)
+    f.limit_gage
+  end
+
+  def lv_text(n)
+    "Lv.#{@model.ships[n].level.to_s}"
+  end
+
+  def money_text
+    "資金 #{@model.money.to_s}"
+  end
+
+  def day_text
+    "#{@model.day.to_s}日目"
+  end
+
+  def time_text
+    "#{@model.hour.to_s}:00"
+  end
+
+  def rate_text
+    "相場 #{@model.rate.to_s}"
+  end
+
+  def ship_caption_text(n)
+    if @model.ships[n].level > 0
+      "改造#{@model.ships[n].levelup_cost.to_s}"
     else
-      ship_status = @model.infishing? ? "作業中" : "停泊中"
+      "購入#{@model.ships[n].levelup_cost.to_s}"
     end
-    Window.drawFont(SHIPS_X[n]+6,SHIPS_Y+10,ship_status,Font16)
+  end
+
+  def ship_state_text(n)
+    if @model.ships[n].level == 0
+      ""
+    elsif @model.infishing?
+      "作業中"
+    else
+      "停泊中"
+    end
   end
 
 end
